a95f055e760b00a1c9d09fb94fb69ccbb829585c
Yuji Naito <y_naito@northtorch.co.jp>
2023-02-03 12:07:05 +0900

    fix: 仮想環境に入らないとシェルスクリプト動作しない不具合修正

--- run_server_01.sh
+++ run_server_01.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/ bash
-SECRET_KEY=`python get_random_secret_key.py`
+SECRET_KEY=`poetry run python get_random_secret_key.py`
 docker run --rm -p 8000:8000 -e SECRET_KEY=$SECRET_KEY --name dd-server-01 dd/server:latest
